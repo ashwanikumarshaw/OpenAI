47effe4791d0bc4d87ba5f355f133c06d3a8a319
Ashwanikumarshaw <Ashwanikumar11shaw@gmail.com>
2022-12-26 21:02:44 +0530

    server

--- client/script.js
+++ client/script.js
@@ -50,7 +50,7 @@ function chatStripe(isAi, value, uniqeId) {
     `
     <div class="wrapper ${isAi && 'ai'}">
         <div class="chat">
-            <div className="profile">
+            <div class="profile">
                 <img
                 src="${isAi ? bot : user}"
                 alt="${isAi ? 'bot' : 'user'}"
@@ -79,7 +79,28 @@ const handleSubmit = async (e) => {
     const messageDiv = document.getElementById(uniqeId);
 
     loader(messageDiv);
+    //fetch data from server -> bot's response
 
+    const response = await fetch('http://localhost:5000', {
+        method: 'POST',
+        headers: { 'Content-Type': 'application/json' },
+        body: JSON.stringify({
+            prompt:data.get('prompt')
+        })
+    })
+
+    clearImmediate(loadInterval);
+    messageDiv.innerHTML = '';
+    if (response.ok) {
+        const data = await response.json();
+        const parsedData = data.bot.trim();
+        typeText(messageDiv, parsedData);
+
+    } else { 
+        const err = await response.text();
+        messageDiv.innerHTML = "Something went wrong";
+
+    }
 }
 
 //we have to call handleSubmit 
